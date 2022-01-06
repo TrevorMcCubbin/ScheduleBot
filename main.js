4e899cfd835587c3e4906b5e9783214aa66fab4e
Trevor McCubbin <mccubal13@gmail.com>
2022-01-06 15:05:53 -0500

    Fix bug where it would always select the first class of the next day

--- main.js
+++ main.js
@@ -37,33 +37,44 @@ function fetchSchedule() {
  */
 function determineNextClass(schedule) {
     const today = new Date();
-    let dayOfWeek = today.getDay();
-    let time = today.getTime();
+    let dayOfWeek = today.getDay() - 1; // -1 so that it works with the json array I set up (monday is 0 instead of 1 in the array)
+    let time = today.getHours() + ":" + today.getMinutes();
     // default is first course of day
     let course = 0;
 
     // For sunday and saturday, fetch the schedule for monday
-    if (dayOfWeek === 0 || dayOfWeek === 6) {
-        dayOfWeek = 1; // set today to monday
+    if (dayOfWeek >= 5) {
+        dayOfWeek = 0; // set today to monday
     } else {
-        // for each class there is in the schedule today
-        schedule[dayOfWeek].forEach((course) => {
+        while (course < schedule[dayOfWeek].length - 1) {
+            let current = schedule[dayOfWeek][course];
             // If the current class finished in 45 minutes or later, go to the next class
-            if (time >= timeCalcs.hoursToMilliseconds(timeCalcs.addTimes(course.time, course.duration)) - timeCalcs.hoursToMilliseconds("00:45")) {
+            if (timeCalcs.timeToMins(time) >= timeCalcs.timeToMins(timeCalcs.addTimes(current.time, current.duration)) - 45) {
                 course++;
+                continue;
             }
             // if we reached the final class today, switch to the first class tomorrow
-            if (course === schedule[dayOfWeek].length - 1) {
+            if (course === schedule[dayOfWeek].length && dayOfWeek < 5) {
                 course = 0;
                 dayOfWeek++;
+                break;
+            } else if (dayOfWeek === 4) { // if it's friday, set next day to monday
+                course = 0;
+                dayOfWeek = 0;
+                break;
             }
+        }
+        // for each class there is in the schedule today
+        schedule[dayOfWeek].forEach((course) => {
+
         });
     }
     let nextClass = schedule[dayOfWeek][course];
+    console.log(nextClass);
     return nextClass;
 }
 
 function setClass(course) {
     client.user.setUsername(course.name);
-    client.user.setActivity(course.classroom);
+    client.user.setActivity(course.classroom + ": " + course.time + "-" + timeCalcs.addTimes(course.time, course.duration));
 }
